414eb8ce0c6480854afe27473abc7954cec63ae8
Santiago <monitocastellani@gmail.com>
2023-03-01 01:11:07 -0300

    m

---
+++ estaciones.js
@@ -0,0 +1,49 @@
+function otoño() {
+    document.getElementById("sobre-mi").style.background = "url('/img/otoño.jpg')";
+    document.getElementById("texto").innerHTML = "Estas en Otoño";
+    document.getElementById("texto").style.color = "green";
+    document.getElementById("titulon").style.color = "red";
+    document.getElementById("titulon").style.background = "green";
+    document.getElementById("titulon").style.borderColor = "yellow";
+    document.getElementById("moon").style.color = "white";
+}
+
+function invierno() {
+    document.getElementById("sobre-mi").style.background = "url('/img/invierno.jpg')";
+    document.getElementById("texto").innerHTML = "Estas en Invierno";
+    document.getElementById("texto").style.color = "blue";
+    document.getElementById("titulon").style.color = "black";
+    document.getElementById("titulon").style.background = "pink";
+    document.getElementById("titulon").style.borderColor = "blue";    
+    document.getElementById("moon").style.color = "black";
+}
+
+function primavera() {
+    document.getElementById("sobre-mi").style.background = "url('/img/flores2.jpg')";
+    document.getElementById("texto").innerHTML = "Estas en Primavera";
+    document.getElementById("texto").style.color = "yellow";
+    document.getElementById("titulon").style.color = "yellow";
+    document.getElementById("titulon").style.background = "black";
+    document.getElementById("titulon").style.borderColor = "red";
+    document.getElementById("moon").style.color = "white";
+}
+
+function verano() {
+    document.getElementById("sobre-mi").style.background = "url('/img/verano.jpg')";
+    document.getElementById("texto").innerHTML = "Estas en Verano";
+    document.getElementById("titulon").style.color = "blue";
+    document.getElementById("titulon").style.background = "yellow";
+    document.getElementById("titulon").style.borderColor = "violet";
+    document.getElementById("texto").style.color = "yellow";
+    document.getElementById("moon").style.color = "black";
+}
+
+function black() {
+    document.getElementById("sobre-mi").style.background = "black";
+    document.getElementById("texto").innerHTML = "Puedes cambiar el modo visual";
+    document.getElementById("titulon").style.color = "orange";
+    document.getElementById("titulon").style.background = "black";
+    document.getElementById("titulon").style.borderColor = "black";
+    document.getElementById("moon").style.color = "white";
+}
+
